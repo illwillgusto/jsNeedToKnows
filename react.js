f7543e10de30e2efe39bc6d5c9dc6590f08675af
Will Walton <will@endless3dge.com>
2024-03-27 09:31:47 -0500

    Update react.js
    examples for how map and filter methods are used in React using JSX

--- react.js
+++ react.js
@@ -24,11 +24,37 @@ const expensiveProducts = products.filter(product => product.price > 600); /* If
 and you want to filter them based on condition. */
 
 console.log(expensiveProducts); // [{ name: 'Laptop', price: 1000 }, { name: 'Tablet', price: 700 }]
+
 /* Map and Filter are essential in React development for promoting immutable data patterns, enabling 
 dynamic and conditional rendering of components, optimizing performance, and contributing to a 
 declarative and maintainable codebase. 
 
-React often involves rendering lists of elements or components based on data 
+React often involves rendering lists of elements or components based on data. The 'map' method is incredibly useful.
+It allows you to transform each item in an array(like data fetched from an API) into a React component. */
+// Example: displaying a list of items where each item is rendered as a component:
+const items = ['Item 1', 'Item 2', 'Item 3'];
+
+return (
+    <ul>
+        {items.map(item => <li key={item}>{item}</li>)}
+    </ul>
+); // this will render an unordered list in your UI, with each item in the 'items' array represented as a list item.
+
+/* Filter method is useful for selectively rendering elements based on certain conditions. Before you map your array to 
+components, you can filter out elements that don't meet certain criteria. */
+// Example: filtering  list of tasks to only show those that are completed: 
+const tasks = [
+    { id: 1, title: 'Task 1', completed: false },
+    { id: 2, title: 'Task 2', completed: true },
+    // more tasks...
+  ];
+  
+  return (
+    <ul>
+      {tasks.filter(task => task.completed).map(task => <li key={task.id}>{task.title}</li>)}
+    </ul>
+  );
+  
 
 // slice & splice method
 
